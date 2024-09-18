e0d23323c7ad7d2f9e81418eb020dc5e401903b3
linconvinicius <lvini_@live.com>
2024-09-18 11:39:48 -0300

    update tests

--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -1,13 +1,20 @@
 const axios = require('axios');
+const crypto = require('crypto');
+const postsService = require('../services/postsService');
+
+const generate = function () {
+    return crypto.randomBytes(20).toString('hex');
+};
 
 test('Should get posts', async function () {
+    const post1 = postsService.savePost({ title: generate(), content: generate() });
+    const post2 = postsService.savePost({ title: generate(), content: generate() });
+    const post3 = postsService.savePost({ title: generate(), content: generate() });
+
     const response = await axios ({
         url: 'http://localhost:3000/posts',
         method: 'get'
     });
     const posts = response.data;
     expect(posts).toHaveLength(3);
-    const [firstPost] = posts;
-    expect(firstPost.id).toBe(1);
-    expect(firstPost.title).toBe('API Rest: Métodos');    
 });
